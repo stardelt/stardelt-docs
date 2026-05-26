98a22a06a8b5966f1ac4030182dde53921e2e251
Martin Stöcker <me@mstoecker.de>
2026-05-26 18:48:26 +0200

    deleting some more ai slop

--- sidebars.ts
+++ sidebars.ts
@@ -14,8 +14,7 @@ const sidebars: SidebarsConfig = {
       collapsed: false,
       items: [
         'getting-started/prerequisites',
-        'getting-started/local-kind',
-        'getting-started/smoke-test',
+        'getting-started/local-kind'
       ],
     },
     {
